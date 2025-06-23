5b6d7b7bcc39f3bbdc8eec08133cb2384dadaad2
Khushi9035 <138421236+Khushi9035@users.noreply.github.com>
2025-06-23 14:35:37 +0530

    Update Footer.js

--- client/src/components/Layout/Footer.js
+++ client/src/components/Layout/Footer.js
@@ -2,9 +2,21 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="bg-dark text-light p-4 ">
-      <h6 className="text-center">All rights reserved &copy; WalletWizard</h6>
-    </div>
+    <>
+      <footer className="footer">
+        <small>All rights reserved &copy; WalletWizard</small>
+      </footer>
+
+      <style>{`
+        .footer {
+          width: 100%;
+          background-color: #1f1f1f;
+          color: white;
+          text-align: center;
+          padding: 1rem 0;
+        }
+      `}</style>
+    </>
   );
 };
 
