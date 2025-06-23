016abf35b81b4627f11461387f04ee20fb9c4956
Khushi9035 <138421236+Khushi9035@users.noreply.github.com>
2025-06-23 14:35:13 +0530

    Update component

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,3 +1,19 @@
+// import React from "react";
+// import Footer from "./Footer";
+// import Header from "./Header";
+
+// const Layout = ({ children }) => {
+//   return (
+//     <>
+//       <Header />
+//       <div className="content">{children}</div>
+//       <Footer />
+//     </>
+//   );
+// };
+
+// export default Layout;
+
 import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
@@ -5,11 +21,44 @@ import Header from "./Header";
 const Layout = ({ children }) => {
   return (
     <>
-      <Header />
-      <div className="content">{children}</div>
-      <Footer />
+      <div className="layout-wrapper">
+        <Header />
+        <div className="layout-body">{children}</div>
+        <Footer />
+      </div>
+
+      <style>{`
+        html, body, #root {
+          height: 100%;
+          margin: 0;
+          padding: 0;
+        }
+
+        .layout-wrapper {
+          display: flex;
+          flex-direction: column;
+          min-height: 100vh;
+        }
+
+        .layout-body {
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+        }
+
+        .layout-body > * {
+          flex: 1;
+        }
+
+        .footer {
+          margin-top: auto;
+        }
+      `}</style>
     </>
   );
 };
 
 export default Layout;
+
+
+
